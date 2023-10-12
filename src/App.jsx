a3e3992681d833d6b65f95d9ca19abb22b25800a
Ellie <eagyeman1234@gmail.com>
2023-10-12 16:09:20 -0400

    add eliies things

--- src/App.jsx
+++ src/App.jsx
@@ -62,6 +62,15 @@ const App = () => {
     },
   ]
 
+  const elliesThings = [
+    {
+      name: "water",
+      image: "https://imgs.xkcd.com/comics/health_drink_2x.png",  
+      attributes: ["efficient", "reusability", "healthy", "good"],
+    },
+  ]
+
+
   return (
     <Routes>
       {/* All the <Route> components should live here */}
